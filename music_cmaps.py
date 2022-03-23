a251997a68792a7f8c9b1490c4bc3c6d021a704b
2wheelsgood <mindyourown@beeswax.bub>
2022-03-22 19:54:24 -0600

    first commit

---
+++ music_cmaps.py
@@ -0,0 +1,14 @@
+import os
+import numpy as np
+from matplotlib.colors import LinearSegmentedColormap
+
+################################################################################
+
+def rgb2cmap(cmapname,ncols):
+    cmarray=np.loadtxt(os.path.dirname(os.path.realpath(__file__))+'/RGB/'+cmapname+'.rgb')
+    cm=LinearSegmentedColormap.from_list(cmapname,cmarray,N=ncols)
+    return cm
+
+kida=rgb2cmap('kida',128)
+    
+    
