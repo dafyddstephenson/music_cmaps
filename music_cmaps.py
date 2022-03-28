a2cffc9c8c1cb893ecd94b8e4f67e01ae219cfee
2wheelsgood <mindyourown@beeswax.bub>
2022-03-28 11:30:20 -0600

    add cmap spiritual state

--- music_cmaps.py
+++ music_cmaps.py
@@ -9,9 +9,10 @@ def rgb2cmap(cmapname,ncols):
     cm=LinearSegmentedColormap.from_list(cmapname,cmarray,N=ncols)
     return cm
 
-kida         = rgb2cmap('kida',128)
-manonthemoon = rgb2cmap('manonthemoon',128)
-punisher     = rgb2cmap('punisher',128)
-fullmoonfever= rgb2cmap('fullmoonfever',128)
-wastinglight = rgb2cmap('wastinglight',128)
-lemonade     = rgb2cmap('lemonade',128)
+kida          = rgb2cmap('kida',128)
+manonthemoon  = rgb2cmap('manonthemoon',128)
+punisher      = rgb2cmap('punisher',128)
+fullmoonfever = rgb2cmap('fullmoonfever',128)
+wastinglight  = rgb2cmap('wastinglight',128)
+lemonade      = rgb2cmap('lemonade',128)
+spiritualstate= rgb2cmap('spiritualstate',128)
